c54361d0a6b1eaee17c5447955e0c18a66d6e1d6
trevor-mccarthy-ilab <78921473+trevor-mccarthy-ilab@users.noreply.github.com>
2025-12-18 11:46:30 +0200

    Add functionality for back and next buttons when viewing timesheets.

--- src/billing_app.py
+++ src/billing_app.py
@@ -10,6 +10,7 @@ Algorythm:
 3. Create a new dataframe using the raw dataframe, processing it with extract_tsheet.py
 4. Allow the user to select customers for timesheet and billing file creation -> save to state
 5. Create timesheet per customer
+    5.1 Allow user to select working customer.
 6. Allow user to set the default download path for each customer's billing files (extract from config.properties)
 7. Display timesheet per customer in tab with a download button -> pdf the timesheet and download the file.
 8. Create and display billing files in tabs with a download button -> xlsx the spreadsheet and downoad the billing file.
@@ -77,7 +78,71 @@ def tsheets_btn(ts, custs): # Pass in raw_tsheet and customers state variables.
         gr.Row.update(visible=True) # nav_buttons
         )  
 
-# tsheet_df, view_output, chkbx_cust, btn_tsheets, btn_download, nav_buttons
+def back_btn(
+        cust,       # curr_cust_state variable
+        ts          # tsheet_df state variable
+        ):
+    
+    # We pass in the currently viewed customer and the dictionary of customer timesheets.
+    # We then cycle backwards through the ts dict, going to the end if we're already at the start.
+
+    # Create an index for the keys in ts.
+    ts_keys = list(ts.keys())
+    index = ts_keys.index(cust)
+
+    # If there is only one customer in ts, do nothing.
+    if len(ts_keys) == 1:
+        return
+
+    # If we are on the first customer in ts, go to the last. Else go to previous customer
+    if index == 0:
+        index = len(ts_keys) - 1
+    else:
+        index = index - 1
+
+    # update cust value with new index.
+    cust = ts_keys[index]
+
+    # retieve dataframe for previous customer.
+    df = ts[cust]
+
+    return(
+        cust,   # curr_cust,
+        df      # view_output
+    )
+
+def next_btn(
+        cust,       # curr_cust_state variable
+        ts          # tsheet_df state variable
+        ):
+    
+    # We pass in the currently viewed customer and the dictionary of customer timesheets.
+    # We then cycle backwards through the ts dict, going to the end if we're already at the start.
+
+    # Create an index for the keys in ts.
+    ts_keys = list(ts.keys())
+    index = ts_keys.index(cust)
+
+    # If there is only one customer in ts, do nothing.
+    if len(ts_keys) == 1:
+        return
+
+    # If we are on the last customer in ts, go to the first. Else go to next customer
+    if index == len(ts_keys) - 1:
+        index = 0
+    else:
+        index = index + 1
+
+    # update cust value with new index.
+    cust = ts_keys[index]
+
+    # retieve dataframe for previous customer.
+    df = ts[cust]
+
+    return(
+        cust,   # curr_cust,
+        df      # view_output
+    )
 
 # End Functions
 ##########################################################
@@ -98,7 +163,7 @@ with gr.Blocks(title='Billing App') as billing_app:
 
     customers = gr.State([])    # The list of customers to create individual timesheets and billing files for.
 
-    curr_cust = gr.State(str)   # hold the name of the currently viewed customer.
+    curr_cust = gr.State()   # hold the name of the currently viewed customer.
 
     raw_tsheet = gr.State(pd.DataFrame())   # The master timesheet created from the uploaded csv.
     tsheet_df = gr.State([]) # list of dataframes containing each tsheet.
@@ -139,7 +204,7 @@ with gr.Blocks(title='Billing App') as billing_app:
                   visible=False
               )
 
-              # Add back and next buttons to cycle through   
+              # 5.1 Add back and next buttons to cycle through   
               with gr.Row(visible=False) as nav_buttons:
                   btn_back = gr.Button(
                       value='Back'
@@ -161,12 +226,7 @@ with gr.Blocks(title='Billing App') as billing_app:
                     wrap=True,
 
                 )
-                 
-
-                
-
-            
-
+ 
 # End GUI
 ##########################################################
 
@@ -214,7 +274,32 @@ with gr.Blocks(title='Billing App') as billing_app:
             btn_tsheets, 
             btn_download, 
             nav_buttons
-            ]
+        ]
+    )
+
+    # 5.1 Navigate through views 
+    btn_back.click(
+        fn=back_btn,
+        inputs=[
+            curr_cust, 
+            tsheet_df,
+        ],
+        outputs=[
+            curr_cust,
+            view_output
+        ]
+    )
+
+    btn_next.click(
+        fn=next_btn,
+        inputs=[
+            curr_cust, 
+            tsheet_df,
+        ],
+        outputs=[
+            curr_cust,
+            view_output
+        ]
     )
 
 # End Event Handlers
