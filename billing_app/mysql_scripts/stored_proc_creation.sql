4d7b07d499a92ab42e3f170fd91c97f78e94eeef
trevor-mccarthy-ilab <78921473+trevor-mccarthy-ilab@users.noreply.github.com>
2026-01-07 12:39:16 +0200

    Add mysql scripts to create database tables and stored procs

---
+++ billing_app/mysql_scripts/stored_proc_creation.sql
@@ -0,0 +1,130 @@
+
+DELIMITER $$
+
+CREATE PROCEDURE si_get_projects_clients()
+BEGIN
+    SELECT
+        proj_client,
+        proj_name
+    FROM projects
+    ORDER BY proj_client, proj_name;
+END$$
+
+CREATE PROCEDURE si_delete_project(
+	IN i_proj_client varchar(50),
+    IN i_proj_name	varchar(50)
+)
+BEGIN
+	DELETE
+    FROM projects
+    WHERE 
+		proj_client = i_proj_client
+	AND
+		proj_name = i_proj_name;
+END $$
+
+CREATE PROCEDURE si_get_project_details(
+	IN i_proj_client varchar(50),
+    IN i_proj_name	varchar(50),
+    out o_proj_client varchar(50),
+    out o_proj_name varchar(50),
+    OUT o_po_num varchar(50),si_delete_projectsi_delete_projectsi_get_project_details
+    OUT o_client_manager varchar(50),
+    out o_ilab_manager varchar(50),
+    out o_job_code varchar(50),
+    out o_manager_sig tinyint(1),
+    out o_employee_sig tinyint(1),
+    out o_notes tinyint(1),
+    out o_weekend json,
+    out o_rate json,
+    out o_po_period varchar(255)
+)
+BEGIN
+	SELECT *
+    FROM projects
+    WHERE 
+		proj_client = i_proj_client
+    AND
+		proj_name = i_proj_name;    
+END $$
+
+
+CREATE PROCEDURE sp_create_employee_insert(
+    in i_f_name varchar(50),
+    in i_l_name varchar(50),
+    in i_proj_client varchar(50),
+    in i_proj_name varchar(50),
+    in i_rate float,
+    in i_emp_role varchar(10)
+)
+BEGIN
+	INSERT INTO employees(
+		f_name,
+		l_name,
+		proj_client,
+		proj_name,
+		rate,
+		emp_role
+    )
+    VALUES(
+		i_f_name,
+		i_l_name,
+		i_proj_client,
+		i_proj_name,
+		i_rate,
+		i_emp_role
+    );
+END $$
+
+CREATE PROCEDURE sp_create_project_insert (
+    IN i_proj_client   VARCHAR(50),
+    IN i_proj_name     VARCHAR(50),
+    IN i_po_num        VARCHAR(50),
+    IN i_client_manager VARCHAR(50),
+    IN i_ilab_manager  VARCHAR(50),
+    IN i_job_code      VARCHAR(50),
+    IN i_manager_sig   BOOLEAN,
+    IN i_employee_sig  BOOLEAN,
+    IN i_notes         BOOLEAN,
+    IN i_weekend       VARCHAR(15),
+    IN i_ot_rate       JSON,
+    IN i_po_start_date VARCHAR(10),
+    IN i_po_end_date   VARCHAR(10)
+)
+BEGIN
+    INSERT INTO projects (
+        proj_client,
+        proj_name,
+        po_num,
+        client_manager,
+        ilab_manager,
+        job_code,
+        manager_sig,
+        employee_sig,
+        notes,
+        weekend,
+        ot_rate,
+        po_start_date,
+        po_end_date
+    )
+    VALUES (
+        i_proj_client,
+        i_proj_name,
+        i_po_num,
+        i_client_manager,
+        i_ilab_manager,
+        i_job_code,
+        i_manager_sig,
+        i_employee_sig,
+        i_notes,
+        i_weekend,
+        i_ot_rate,
+        i_po_start_date,
+        i_po_end_date
+    );
+
+END$$
+
+
+
+DELIMITER ;
---
+++ billing_app/mysql_scripts/table_creation.sql
@@ -0,0 +1,60 @@
+
+
+drop table projects;
+
+CREATE TABLE projects (
+
+    id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    proj_client    VARCHAR(50) NOT NULL,
+	proj_name      VARCHAR(50) NOT NULL,
+
+
+    -- Other columns
+    po_num         VARCHAR(50) NOT NULL,
+    client_manager   VARCHAR(50) NOT NULL,
+    ilab_manager	VARCHAR(50) NOT NULL,
+    job_code       VARCHAR(50) NOT NULL,
+
+    manager_sig    BOOLEAN NOT NULL,
+    employee_sig   BOOLEAN NOT NULL,
+    notes          BOOLEAN NOT NULL,
+
+    weekend        VARCHAR(15) NOT NULL,
+    ot_rate        JSON NOT NULL,
+
+    po_start_date      VARCHAR(10) NOT NULL,
+    po_end_date		varchar(10) not null,
+
+    -- Additional indexes
+    INDEX idx_proj_client (proj_client),
+    INDEX idx_proj_name (proj_name),
+    INDEX idx_job_code (job_code),
+    INDEX idx_ilab_manager (ilab_manager),
+    INDEX idx_po_num (po_num)
+)
+ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+drop table employees;
+
+CREATE TABLE employees(
+	id integer unsigned auto_increment not null primary key,
+    f_name varchar(50) not null,
+    l_name varchar(50) not null,
+    proj_client varchar(50) not null,
+    proj_name varchar(50) not null,
+    rate float not null,
+    emp_role varchar(10) not null,
+    
+    -- Additional indexes
+    INDEX idx_proj_client (proj_client),
+    INDEX idx_proj_name (proj_name)
+)
+ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+
+
